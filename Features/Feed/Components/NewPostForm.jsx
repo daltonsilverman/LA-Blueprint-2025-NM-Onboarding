88a10cd9e26d1b9418ff9f137ba0d1805762dcf0
daltonsilverman <daltonsilverman04@gmail.com>
2024-11-26 14:37:46 -0800

    milestone 2 complete

--- Features/Feed/Components/NewPostForm.jsx
+++ Features/Feed/Components/NewPostForm.jsx
@@ -2,32 +2,33 @@ import { Button, TextInput } from 'react-native';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export default function NewPostForm() {
-    const [username, setUsername] = useState('');
-    const [body, setBody] = useState('')
+export default function NewPostForm({ addNewPost }) {
+  const [username, setUsername] = useState('');
+  const [body, setBody] = useState('');
 
-    const handleAddPost = () => {
-      addNewPost({ username, body });
-      setUsername('');
-      setBody('');
-    }
+  const handleAddPost = () => {
+    addNewPost({ username, body });
+    setUsername('');
+    setBody('');
+  };
 
-
-    return (
-        <>
-            <TextInput
-              placeholder="Who is this?"
-              onChangeText={setUsername}
-            />
-            <TextInput
-              placeholder="What are you Printing?"
-              onChangeText={setBody}
-            />
-            <Button title="Print" />
-        </>
-    );
+  return (
+    <>
+      <TextInput
+        placeholder="Who is this?"
+        onChangeText={setUsername}
+        value={username}
+      />
+      <TextInput
+        placeholder="What are you Printing?"
+        onChangeText={setBody}
+        value={body}
+      />
+      <Button title="Print" onPress={handleAddPost} />
+    </>
+  );
 }
 
 NewPostForm.propTypes = {
   addNewPost: PropTypes.func.isRequired,
-}
+};
--- Features/Feed/Screens/Feed.jsx
+++ Features/Feed/Screens/Feed.jsx
@@ -1,8 +1,8 @@
 import { Button, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
-import Post from '../Components/Post'
+import { useState } from 'react';
+import Post from '../Components/Post';
 import NewPostForm from '../Components/NewPostForm';
-import { useState, useEffect } from 'react';
 
 export default function Feed({ navigation }) {
   const [posts, setPosts] = useState(
@@ -11,31 +11,30 @@ export default function Feed({ navigation }) {
         _id: 1,
         username: 'James',
         body: 'Mobile development is fun!',
-        //time: '2023-10-24T00:41:59.057Z',
+        // time: '2023-10-24T00:41:59.057Z',
       },
       {
         _id: 2,
         username: 'Sidd',
         body: 'I just finished watching another movie. It was interesting, but kind of boring :(',
-        //time: '2023-10-23T00:41:59.057Z',
+        // time: '2023-10-23T00:41:59.057Z',
       },
       {
         _id: 3,
         username: 'Jerry',
         body: 'I am excited to see everyone become friends!',
-        //time: '2023-10-25T00:41:59.057Z',
+        // time: '2023-10-25T00:41:59.057Z',
       },
-    ]    
+    ],
   );
   const [newId, setNewId] = useState(0);
 
   const addNewPost = (newPost) => {
     const updatedPosts = [...posts];
-    updatesPosts.push({...newPost, _id: newId });
+    updatedPosts.push({ ...newPost, _id: newId });
     setPosts(updatedPosts);
     setNewId((id) => (id + 1));
   };
-
 
   const navigateToLanding = () => {
     navigation.navigate('Landing');
@@ -46,7 +45,7 @@ export default function Feed({ navigation }) {
       <Text>Posts</Text>
       <NewPostForm addNewPost={addNewPost} />
       {posts.map((post) => (
-        <Post key={post._id} username={post.username} body={post.body}/>
+        <Post key={post._id} username={post.username} body={post.body} />
       ))}
       <Button
         title="To Landing"
