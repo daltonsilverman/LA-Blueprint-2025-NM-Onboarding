4f27a245cbe9bdbb7c630f16cb9c9ef4ceb45ff5
daltonsilverman <daltonsilverman04@gmail.com>
2024-11-26 17:12:39 -0800

    Some stylization

--- Features/Feed/Components/NewPostForm.jsx
+++ Features/Feed/Components/NewPostForm.jsx
@@ -1,6 +1,34 @@
-import { Button, TextInput } from 'react-native';
+import {
+  Button, TextInput, View, StyleSheet,
+} from 'react-native';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    borderColor: 'lightgrey',
+    backgroundColor: 'ghostwhite',
+    width: '100%',
+    height: 100,
+    paddingHorizontal: 10,
+    borderBottomWidth: 5,
+    borderTopWidth: 1,
+    alignItems: 'flex-start',
+    alignContent: 'space-evenly',
+    justifyContent: 'space-evenly',
+  },
+  rowContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  printButton: {
+    color: '#008AD8',
+  },
+});
 
 export default function NewPostForm({ addNewPost }) {
   const [username, setUsername] = useState('');
@@ -13,19 +41,22 @@ export default function NewPostForm({ addNewPost }) {
   };
 
   return (
-    <>
+    <View style={styles.formContainer}>
       <TextInput
-        placeholder="Who is this?"
-        onChangeText={setUsername}
-        value={username}
-      />
-      <TextInput
-        placeholder="What are you Printing?"
+        placeholder="What's Happening?"
         onChangeText={setBody}
         value={body}
       />
-      <Button title="Print" onPress={handleAddPost} />
-    </>
+      <View style={styles.rowContainer}>
+        <TextInput
+          placeholder="Who's this?"
+          onChangeText={setUsername}
+          value={username}
+        />
+        <Button title="Print" onPress={handleAddPost} color="#008AD8" />
+      </View>
+
+    </View>
   );
 }
 
--- Features/Feed/Components/Post.jsx
+++ Features/Feed/Components/Post.jsx
@@ -1,24 +1,48 @@
 // Features/Feed/Components/Post.jsx
-import { Text } from 'react-native';
+import {
+  Text, View, StyleSheet, Button,
+} from 'react-native';
 import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+  postContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    borderColor: 'lightgrey',
+    backgroundColor: 'ghostwhite',
+    width: '100%',
+    paddingHorizontal: 50,
+    borderBottomWidth: 1,
+  },
+  authorText: {
+    fontWeight: 'bold',
+    paddingVertical: 10,
+  },
+
+  bodyText: {
+    paddingBottom: 25,
+  },
+});
 
 export default function Post({ username, body }) {
   return (
-    <>
-        <Text>
-            Author:
-            {username}
+    <View style={styles.postContainer}>
+      <Text style={styles.authorText}>
+        {username}
+      </Text>
+      <View>
+        <Text style={styles.bodyText}>
+          {body}
         </Text>
-        <Text>
-            Body:
-            {body}
-        </Text>
-    </>
+        <Button title="+" />
+      </View>
+
+    </View>
   );
 }
 
 Post.propTypes = {
-    username: PropTypes.string.isRequired,
-    body: PropTypes.string.isRequired,
-    //time: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  // time: PropTypes.string.isRequired,
 };
